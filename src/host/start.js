10f5d031535b0114c72356eb4d17f23b2626c8c2
Oushan Skeen <inta.soul@gmail.com>
2020-07-25 15:16:29 +0300

    Turn off default local ip enabled

--- src/host/start.js
+++ src/host/start.js
@@ -2,6 +2,6 @@ const app = require('./server');
 
 //  port ------------------------------------------------------------
     const port = process.env.PORT || /*80*/4001;
-    //app.listen(port/*, "142.93.173.95"*/);
-    app.listen(port, "127.0.0.1");
+    app.listen(port/*, "142.93.173.95"*/);
+   // app.listen(port, "127.0.0.1");
     console.log("dodoc RESTful API started on port: " + port);
